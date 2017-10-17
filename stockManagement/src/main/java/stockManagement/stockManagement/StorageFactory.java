1f29c39793a780f76c24f4e218f02eec55306b57
Daniel <ertl97@gmx.at>
2017-10-17 21:42:39 +0200

    JavaDoc Factory

--- stockManagement/src/main/java/stockManagement/stockManagement/StorageFactory.java
+++ stockManagement/src/main/java/stockManagement/stockManagement/StorageFactory.java
@@ -2,9 +2,19 @@ package stockManagement.stockManagement;
 
 import stockManagement.Data.InMemory;
 import stockManagement.Data.Storage;
-
+/**
+ * Die Klasse wird zur Erstellung eines Storage Objekts verwendet.
+ * @author Daniel
+ *
+ */
 public class StorageFactory {
 	
+	
+	/**
+	 * In dieser Methode wird abgefragt welche Speicher möglichkeit man auswählt.
+	 * @param storageType
+	 * @return Storage
+	 */
 	public static Storage getStorage(String storageType) {
 		if(storageType == null){
 			return null;
